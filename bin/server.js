1d89157899dd33ca16959bee93bd524759eb8463
Sandelier <sandelier.fakeemail@gmail.com>
2023-08-25 07:45:40 +0300

    Initial stage.

---
+++ bin/server.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+
+// Server is used so that we can monitor files and then reload the extension.
+const server = new WebSocket.Server({ port: 46532 });
+const clients = new Set();
+
+server.on('connection', (socket) => {
+    console.log('Client connected');
+    clients.add(socket);
+    
+    socket.on('message', (message) => {
+        console.log(`Received: ${message}`);
+    });
+    
+    socket.send('Hello, client!');
+});
+
+function sendMessage(message) {
+    clients.forEach(client => {
+        client.send(JSON.stringify(message));
+    });
+}
+
+let debounceTimer;
+fs.watch(folderPath, (eventType, filename) => {
+    if (eventType === 'change') {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => {
+            sendMessage({ from: 'web-forge', function: 'reloadExtension' });
+        }, 500);
+    }
+});
