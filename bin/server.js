24246d9f90d0f314c6c04d0be2739a4c1e9d3902
sandelier <sandelier.fakegmail@gmail.com>
2023-08-27 09:05:43 +0300

    Added folder watch. Fixed temp folder removing.

--- bin/server.js
+++ bin/server.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws');
 const fs = require('fs');
 const { EventEmitter } = require('events');
 const eventEmitter = new EventEmitter();
+const crypto = require('crypto');
 
 
 // Server is used so that we can monitor files and then reload the extension.
@@ -22,16 +23,82 @@ function createServer(folderWatch) {
             eventEmitter.emit('socketClosed');
         });
     });
-    
+
+    const previousChecksums = {};
+
+    initializeChecksums(folderWatch);
+    // Initilizing them so we can have all current checksums right away.
+    function initializeChecksums(directory) {
+        const items = fs.readdirSync(directory);
+        for (const item of items) {
+            const itemPath = `${directory}/${item}`;
+            const stats = fs.statSync(itemPath);
+            if (stats.isFile()) {
+                previousChecksums[itemPath] = calculateFileChecksum(itemPath);
+            } else if (stats.isDirectory()) {
+                initializeChecksums(itemPath);
+            }
+        }
+    }
+
+    let isFirstEvent = true;
     let debounceTimer;
-    fs.watch(folderWatch, (eventType, filename) => {
-        if (eventType === 'change') {
+
+    fs.watch(folderWatch, { recursive: true }, (eventType, filename) => {
+        // First event is for subdirectorys because fs.watch triggers multiple event changes one for subdirectory and one for the parent directory.
+        if (isFirstEvent) {
+            // debouncetimer used because multiple events triggered per change.
             clearTimeout(debounceTimer);
             debounceTimer = setTimeout(() => {
-                sendMessage({ from: 'web-forge', function: 'reloadExtension' });
-            }, 500);
+                isFirstEvent = true; 
+            }, 100);
+
+            const filePath = `${folderWatch}/${filename}`;
+            console.log(filePath);
+            console.log(eventType);
+            if (eventType === 'change') {
+                handleFileChange(filePath);
+            } else if (eventType === 'rename') {
+                if (!fs.existsSync(filePath)) {
+                    console.log("File or folder removed:", filePath);
+                    deleteChecksumsRecursively(filePath);
+                } else {
+                    const stats = fs.statSync(filePath);
+                    if (stats.isFile()) {
+                        handleFileChange(filePath);
+                    } else if (stats.isDirectory()) {
+                        console.log("Folder added or renamed:", filePath);
+                        deleteChecksumsRecursively(filePath);
+                    }
+                }
+            }
+            isFirstEvent = false;
         }
     });
+
+    function handleFileChange(filePath) {
+        const currentChecksum = calculateFileChecksum(filePath);
+
+        if (previousChecksums[filePath] && previousChecksums[filePath] !== currentChecksum) {
+            console.log("File contents changed:", filePath);
+        }
+
+        previousChecksums[filePath] = currentChecksum;
+    }
+
+    // Used so we know if the checksum is different = file content changed.
+    function calculateFileChecksum(filePath) {
+        const fileData = fs.readFileSync(filePath);
+        return crypto.createHash('md5').update(fileData).digest('hex');
+    }
+
+    function deleteChecksumsRecursively(directory) {
+        for (const key in previousChecksums) {
+            if (key.startsWith(directory)) {
+                delete previousChecksums[key];
+            }
+        }
+    }
 }
 
 module.exports = {
